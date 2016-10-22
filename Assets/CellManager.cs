477657fa16ab4d147cdb79aad8e81d3bc145b938
Eric <EricMouledoux@Gmail.com>
2016-10-22 10:10:53 -0500

    Organ types

--- Assets/CellManager.cs
+++ Assets/CellManager.cs
@@ -1,7 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CellManager : MonoBehaviour
+public static class CellManager
 {
-    public int cells;
+    public static int cells;
+    public static int people;
+
+    public static Organ skin;
+    public static Organ kidnies;
+    public static Organ livers;
+    public static Organ bones;
+    public static Organ lungs;
+    public static Organ hearts;
+    public static Organ brains;
+    public static Organ eyes;
+}
+
+public class Organ
+{
+    public int numberOfUnits;
+    public int pricePerUnit;
+    public int cellsPerTick;
 }
