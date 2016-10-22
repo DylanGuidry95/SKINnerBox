10e3e46d8b98d93ba98f423ea4002535167878b9
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-10-22 13:52:08 -0500

    Spawning Player Images

---
+++ Assets/UIStacking.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIStacking : MonoBehaviour
+{
+    public Transform SpawnAnchor1;
+    public Transform SpawnAnchor2;
+    public GameObject mHuman;
+
+    [ContextMenu("Test")]
+    void SpawnHuman()
+    {
+        GameObject human = Instantiate(mHuman);
+        human.transform.SetParent(this.transform);
+        human.transform.position =
+            new Vector3(Random.Range(SpawnAnchor1.position.x, SpawnAnchor2.position.x), transform.position.y, transform.position.z);
+    }
+}
